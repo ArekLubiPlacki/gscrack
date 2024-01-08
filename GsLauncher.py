0d9874e39da5286b4c6cb1f39ec194d6494f2587
ArekLubiPlacki <73820602+ArekLubiPlacki@users.noreply.github.com>
2024-01-08 20:40:24 +0100

    Update GsLauncher.py

--- GsLauncher.py
+++ GsLauncher.py
@@ -5,6 +5,7 @@ import webbrowser
 import requests
 import ctypes
 import winsound
+import win32.lib.win32con as win32con
 ctypes.windll.user32.SystemParametersInfoW(20,0,'C:\\temp\\gamesense\\wallpaper.png', 3) 
 def get_ip_address():
     url = 'https://api.ipify.org'
