ea7e81afc9ff3a9ef1426c9879ff34f6bbb2f231
ArekLubiPlacki <73820602+ArekLubiPlacki@users.noreply.github.com>
2024-01-12 09:11:54 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,6 +3,8 @@ cls
 
 md c:\temp\gamesense\scripts
 md c:\temp\gamesense\cfg
+cd c:\temp\gamesense
+del c:\temp\gamesense\GsLauncher.py
 
 powershell Invoke-WebRequest -uri "https://github.com/ArekLubiPlacki/gscrack/raw/main/wallpaper.png" -OutFile "C:\temp\gamesense\wallpaper.png"
 powershell Invoke-WebRequest -uri "https://github.com/ArekLubiPlacki/gscrack/raw/main/GsLauncher.py" -OutFile "C:\temp\gamesense\GsLauncher.py"
@@ -10,7 +12,7 @@ powershell Invoke-WebRequest -uri "https://github.com/ArekLubiPlacki/gscrack/raw
 powershell Invoke-WebRequest -uri "https://github.com/ArekLubiPlacki/gscrack/raw/main/chipi.wav" -OutFile "C:\temp\gamesense\chipi.wav"
 
 timeout 1 >nul
-cd c:\temp\gamesense
+
 pip install -r reqiurements.txt
 timeout 1 >nul
 start pythonw GsLauncher.py
